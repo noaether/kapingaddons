c8771b54235bf2b21fe5bef6ea080cb7acc86603
noadev <67566122+thenoadev@users.noreply.github.com>
2023-01-07 18:03:10 -0500

    added commands and formatted onEnable()

--- src/main/java/ca/noae/kapingaddons/PluginMain.java
+++ src/main/java/ca/noae/kapingaddons/PluginMain.java
@@ -5,9 +5,11 @@ import org.bukkit.event.Listener;
 
 import ca.noae.kapingaddons.Handlers.PermissionHandler;
 import ca.noae.kapingaddons.Handlers.ConfigHandler;
+import ca.noae.kapingaddons.Handlers.InventoryHandler;
 import ca.noae.kapingaddons.Handlers.LogHandler;
 import ca.noae.kapingaddons.Handlers.MessageHandler;
-
+import ca.noae.kapingaddons.Commands.Admin.AdminMode;
+import ca.noae.kapingaddons.Commands.Admin.Vanish;
 import ca.noae.kapingaddons.Commands.Gamemode.Adventure;
 import ca.noae.kapingaddons.Commands.Gamemode.Creative;
 import ca.noae.kapingaddons.Commands.Gamemode.Spectator;
@@ -21,23 +23,27 @@ public final class PluginMain extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        instance = this;
-        this.saveDefaultConfig();
+            instance = this;
+            this.saveDefaultConfig();
         getInstance().getLogger().info("Kapinga Kalunda arrive ! Cachez vous !");
 
-        getServer().getPluginManager().registerEvents(this, this);
-        Objects.requireNonNull(this.getCommand("gma")).setExecutor(new Adventure(this));
-        Objects.requireNonNull(this.getCommand("gmc")).setExecutor(new Creative(this));
-        Objects.requireNonNull(this.getCommand("gmsp")).setExecutor(new Spectator(this));
-        Objects.requireNonNull(this.getCommand("gms")).setExecutor(new Survival(this));
-        getInstance().getLogger().info("CommandExecutors; they do be executing");
-
+            getServer().getPluginManager().registerEvents(this, this);
         getInstance().getLogger().info("EventListeners; they do be listening");
 
-        new ConfigHandler(this);
-        new PermissionHandler(this);
-        new LogHandler(this);
-        new MessageHandler(this);
+            Objects.requireNonNull(this.getCommand("gma")).setExecutor(new Adventure(this));
+            Objects.requireNonNull(this.getCommand("gmc")).setExecutor(new Creative(this));
+            Objects.requireNonNull(this.getCommand("gmsp")).setExecutor(new Spectator(this));
+            Objects.requireNonNull(this.getCommand("gms")).setExecutor(new Survival(this));
+
+            Objects.requireNonNull(this.getCommand("adminmode")).setExecutor(new AdminMode(this));
+            Objects.requireNonNull(this.getCommand("vanish")).setExecutor(new Vanish(this));
+        getInstance().getLogger().info("CommandExecutors; they do be executing");
+
+            new ConfigHandler(this);
+            new PermissionHandler(this);
+            new LogHandler(this);
+            new MessageHandler(this);
+            new InventoryHandler(this);
         getInstance().getLogger().info("Handlers; they do be handling");
 
         getInstance().getLogger().info("Noae (c) 2022-2023");
