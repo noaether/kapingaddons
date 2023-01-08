ddede8d008189dbc348cf408bdcd0f8fc818902b
noadev <67566122+thenoadev@users.noreply.github.com>
2023-01-07 23:55:16 -0500

    finalized vanish command

--- src/main/java/ca/noae/kapingaddons/Commands/Admin/Vanish.java
+++ src/main/java/ca/noae/kapingaddons/Commands/Admin/Vanish.java
@@ -1,11 +1,15 @@
 package ca.noae.kapingaddons.Commands.Admin;
 
 import ca.noae.kapingaddons.Handlers.ConfigHandler;
+import ca.noae.kapingaddons.Handlers.LogHandler;
 import ca.noae.kapingaddons.Handlers.MessageHandler;
+import ca.noae.kapingaddons.Handlers.PlayerHandler;
 import ca.noae.kapingaddons.PluginMain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
@@ -18,40 +22,39 @@ import org.bukkit.entity.Player;
 
 public class Vanish implements CommandExecutor {
     private final PluginMain plugin;
-    private List<UUID> vanished = new ArrayList<>();
+    public Set<UUID> vanished;
 
     public Vanish(PluginMain plugin) {
         this.plugin = plugin;
+        this.vanished = new HashSet<>();
     }
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(sender instanceof ConsoleCommandSender) {
-            MessageHandler.Error.sendNoConsole();
+        if (!(sender instanceof Player)) {
+            // The sender is not a player, so return
             return true;
         }
+
         Player player = (Player) sender;
-        if (player.hasPermission("stuff.vanish")) {
 
-            if (vanished.contains(player.getUniqueId())) { // their are vanished
-                vanished.remove(player.getUniqueId());
-                for (Player target : Bukkit.getOnlinePlayers()) {
-                    target.showPlayer(plugin, player);
-                }
-                MessageHandler.InGame.sendMessage((Player) sender,
-                        ConfigHandler.messageConfig.getString("messages.vanish.off"));
-            } else { // not vanished
-                vanished.add(player.getUniqueId());
-
-                for (Player target : Bukkit.getOnlinePlayers()) {
-                    target.hidePlayer(plugin, player);
-                }
-
-                MessageHandler.InGame.sendMessage((Player) sender,
-                        ConfigHandler.messageConfig.getString("messages.vanish.on"));
+        if (PlayerHandler.VanishedPlayers.isVanished(player))  {
+            // The player is vanished, so unvanish them
+            PlayerHandler.VanishedPlayers.removePlayer(player);
+            for (Player target : Bukkit.getOnlinePlayers()) {
+                target.showPlayer(plugin, player);
             }
+            MessageHandler.InGame.sendMessage(player, ConfigHandler.messageConfig.getString("messages.unvanish"));
+            LogHandler.Console.info(player.getName() + " unvanished");
         } else {
-            MessageHandler.Error.sendNoPermission((Player) sender);
+            // The player is not vanished, so vanish them
+            PlayerHandler.VanishedPlayers.addPlayer(player);
+            for (Player target : Bukkit.getOnlinePlayers()) {
+                target.hidePlayer(plugin, player);
+            }
+            MessageHandler.InGame.sendMessage(player, ConfigHandler.messageConfig.getString("messages.vanish"));
+            LogHandler.Console.info(player.getName() + " vanished");
         }
+
         return true;
     }
 }
--- src/main/java/ca/noae/kapingaddons/PluginMain.java
+++ src/main/java/ca/noae/kapingaddons/PluginMain.java
@@ -5,15 +5,17 @@ import org.bukkit.event.Listener;
 
 import ca.noae.kapingaddons.Handlers.PermissionHandler;
 import ca.noae.kapingaddons.Handlers.ConfigHandler;
-import ca.noae.kapingaddons.Handlers.InventoryHandler;
+import ca.noae.kapingaddons.Handlers.FileHandler;
 import ca.noae.kapingaddons.Handlers.LogHandler;
 import ca.noae.kapingaddons.Handlers.MessageHandler;
+
 import ca.noae.kapingaddons.Commands.Admin.AdminMode;
 import ca.noae.kapingaddons.Commands.Admin.Vanish;
 import ca.noae.kapingaddons.Commands.Gamemode.Adventure;
 import ca.noae.kapingaddons.Commands.Gamemode.Creative;
 import ca.noae.kapingaddons.Commands.Gamemode.Spectator;
 import ca.noae.kapingaddons.Commands.Gamemode.Survival;
+import ca.noae.kapingaddons.EventListeners.PlayerConnections;
 
 import java.util.Objects;
 
@@ -24,11 +26,23 @@ public final class PluginMain extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
             instance = this;
+            if (!getDataFolder().exists()) getDataFolder().mkdir();
             this.saveDefaultConfig();
         getInstance().getLogger().info("Kapinga Kalunda arrive ! Cachez vous !");
 
+
+        PlayerConnections playerConnections = new PlayerConnections(this);
+
             getServer().getPluginManager().registerEvents(this, this);
+            getServer().getPluginManager().registerEvents(playerConnections, this);
         getInstance().getLogger().info("EventListeners; they do be listening");
+
+            new ConfigHandler(this);
+            new PermissionHandler(this);
+            new LogHandler(this);
+            new MessageHandler(this);
+            new FileHandler(this);
+        getInstance().getLogger().info("Handlers; they do be handling");
 
             Objects.requireNonNull(this.getCommand("gma")).setExecutor(new Adventure(this));
             Objects.requireNonNull(this.getCommand("gmc")).setExecutor(new Creative(this));
@@ -39,12 +53,7 @@ public final class PluginMain extends JavaPlugin implements Listener {
             Objects.requireNonNull(this.getCommand("vanish")).setExecutor(new Vanish(this));
         getInstance().getLogger().info("CommandExecutors; they do be executing");
 
-            new ConfigHandler(this);
-            new PermissionHandler(this);
-            new LogHandler(this);
-            new MessageHandler(this);
-            new InventoryHandler(this);
-        getInstance().getLogger().info("Handlers; they do be handling");
+        // create two folders in the plugin folder
 
         getInstance().getLogger().info("Noae (c) 2022-2023");
 
